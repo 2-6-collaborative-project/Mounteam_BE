e8dfeb9fcd6c1575b851d2ed5bfc370a355ddd2d
black2code <alsdnr1080@gmail.com>
2024-03-24 10:42:27 +0900

    Fix : security 해제

--- src/main/java/com/example/mountain/MountainApplication.java
+++ src/main/java/com/example/mountain/MountainApplication.java
@@ -2,12 +2,14 @@ package com.example.mountain;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.stereotype.Controller;
 
-@SpringBootApplication
+
 @Controller
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class MountainApplication {
 
 	public static void main(String[] args) {
