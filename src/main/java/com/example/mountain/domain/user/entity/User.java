45e0514f24ee2e36283094d688736ab4846907ab
흑이 <93053451+black2code@users.noreply.github.com>
2024-03-28 01:23:04 +0900

    Update User.java

--- src/main/java/com/example/mountain/domain/user/entity/User.java
+++ src/main/java/com/example/mountain/domain/user/entity/User.java
@@ -31,7 +31,6 @@ import java.util.stream.Collectors;
 public class User extends BaseEntity {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long userId;
 
     private String userAccount;
