dc85988f6ec72d704672e1e70874e55bb8e0a1eb
leemimi <mijeong1015@naver.com>
2024-04-01 15:19:28 +0900

    fix: 중복 파일 제거

--- src/main/java/com/example/mountain/domain/team/dto/request/TeamCreateRequest.java
+++ src/main/java/com/example/mountain/domain/team/dto/request/TeamCreateRequest.java
@@ -4,9 +4,6 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.ToString;
-
-import java.time.LocalDateTime;
 import java.util.List;
 
 @Builder
