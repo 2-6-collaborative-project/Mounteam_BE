61c46fa3edd2e7d8e2cf06d4da0f2b63bf0b03e6
black2code <alsdnr1080@gmail.com>
2024-04-03 05:12:03 +0900

    feat : 내 피드 보기, 무한스크롤

--- src/main/java/com/example/mountain/domain/feed/repository/FeedRepositoryCustom.java
+++ src/main/java/com/example/mountain/domain/feed/repository/FeedRepositoryCustom.java
@@ -7,4 +7,5 @@ import org.springframework.data.domain.Slice;
 public interface FeedRepositoryCustom{
 
     Slice<FeedListResponse> findAllFeed(Pageable pageable, Long userId);
+    Slice<FeedListResponse> getImagesInFeeds(Long userId, Pageable pageable, Long cursorId);
 }
--- src/main/java/com/example/mountain/domain/feed/repository/FeedRepositoryImpl.java
+++ src/main/java/com/example/mountain/domain/feed/repository/FeedRepositoryImpl.java
@@ -4,30 +4,27 @@ import com.example.mountain.domain.feed.dto.response.FeedListResponse;
 import com.example.mountain.domain.feed.entity.Feed;
 import static com.example.mountain.domain.feed.entity.QFeed.feed;
 
+import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
-import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Slice;
+import org.springframework.data.domain.*;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 public class FeedRepositoryImpl implements FeedRepositoryCustom {
-    private final EntityManager em;
+
+    private final JPAQueryFactory jpaQueryFactory;
 
     @Override
-    public Slice<FeedListResponse> findAllFeed (Pageable pageable, Long userId) {
-        JPAQueryFactory queryFactory = new JPAQueryFactory(em);
+    public Page<FeedListResponse> findAllFeed (Pageable pageable, Long userId) {
 
-        JPAQuery<Feed> countQuery = queryFactory.selectFrom(feed);
+        JPAQuery<Feed> countQuery = jpaQueryFactory.selectFrom(feed);
         long total = countQuery.fetchCount();
 
-        List<Feed> feeds = queryFactory.selectFrom(feed)
+        List<Feed> feeds = jpaQueryFactory.selectFrom(feed)
                 .orderBy(feed.createDate.desc())
                 .offset(pageable.getOffset())
                 .limit(pageable.getPageSize())
@@ -40,4 +37,30 @@ public class FeedRepositoryImpl implements FeedRepositoryCustom {
         return new PageImpl<>(feedListResponses, pageable, total);
     }
 
+    @Override
+    public Slice<FeedListResponse> getImagesInFeeds(Long userId, Pageable pageable, Long cursorId) {
+        List<Feed> content = jpaQueryFactory
+                .select(feed)
+                .from(feed)
+                .where(feed.user.userId.eq(userId),
+                        gtTeamId(cursorId))
+                .orderBy(feed.id.desc())
+                .limit(pageable.getPageSize() +1 )
+                .fetch();
+
+        List<FeedListResponse> feedListResponses = content.stream()
+                .map(feed -> FeedListResponse.from(feed, userId))
+                .collect(Collectors.toList());
+
+        boolean hasNext = false;
+        if (content.size() > pageable.getPageSize()) {
+            content.remove(pageable.getPageSize());
+            hasNext = true;
+        }
+
+        return new SliceImpl<>(feedListResponses, pageable, hasNext);
+    }
+    private BooleanExpression gtTeamId(Long cursorId) {
+        return cursorId == null ? null : feed.id.gt(cursorId);
+    }
 }
